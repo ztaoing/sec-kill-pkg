d64d190717b7ce2578fc7f805121c61e94c7b66d
tao <1216707759@qq.com>
2020-12-06 09:06:20 +0800

    edit packet name

--- pkg/client/oauth.go
+++ pkg/client/oauth.go
@@ -8,9 +8,9 @@ package client
 import (
 	"context"
 	"github.com/opentracing/opentracing-go"
-	"secondkill/pb"
-	"secondkill/pkg/discover"
-	"secondkill/pkg/loadbalance"
+	"sec-kill-pkg/pb"
+	"sec-kill-pkg/pkg/discover"
+	"sec-kill-pkg/pkg/loadbalance"
 )
 
 type OAuthClient interface {
